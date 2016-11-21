17d0451bc313b4381799320b9620c2a3bdd7380e
John Ky <newhoggy@gmail.com>
2016-11-21 21:51:24 +1100

    Send datagram

--- src/HaskellWorks/Statsd.hs
+++ src/HaskellWorks/Statsd.hs
@@ -1,7 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HaskellWorks.Statsd where
+
+import           Network.Socket
+import qualified Network.Socket.ByteString as NSBS
 
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
 
 main :: IO ()
 main = do
+  addrinfos <- getAddrInfo Nothing (Just "localhost") (Just "34567")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+  _ <- NSBS.sendTo sock "omsg" (addrAddress serveraddr)
   return ()
